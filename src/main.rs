27d1ed1e07e0b86bf6395126846a83fd4a517f57
jeandeducla <jean.deducla@gmail.com>
2022-05-12 17:25:06 +0200

    Examples work but we don't consider going backwards or shortcutting pass the target and then backward

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 fn minimum_path(target: usize, shortcuts: &[usize]) -> usize {
-    0
+    let mut min_path = 0;
+    let mut current_node = 0;
+    loop {
+        if current_node == target {
+            return min_path;
+        }
+
+        let shortcut = shortcuts.get(current_node).unwrap() - 1;
+        if (shortcut <= target) && (shortcut != current_node) {
+            current_node = shortcut;
+            min_path += 1;
+            continue;
+        }
+
+        current_node += 1;
+        min_path += 1;
+    }
 }
 
 fn minimum_paths(n: usize, shortcuts: &[usize]) -> Vec<usize> {
@@ -15,5 +31,34 @@ fn main() {
     let n = 3;
     let shortcuts = vec![2, 2, 3];
 
-    println!("mike's minimum paths: {:?}", minimum_paths(n, &shortcuts));
+    println!("{:?}", minimum_paths(n, &shortcuts));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let n = 3;
+        let shortcuts = vec![2, 2, 3];
+
+        assert_eq!(vec![0, 1, 2], minimum_paths(n, &shortcuts));
+    }
+
+    #[test]
+    fn example2() {
+        let n = 5;
+        let shortcuts = vec![1, 2, 3, 4, 5];
+
+        assert_eq!(vec![0, 1, 2, 3, 4], minimum_paths(n, &shortcuts));
+    }
+
+    #[test]
+    fn example3() {
+        let n = 7;
+        let shortcuts = vec![4, 4, 4, 4, 7, 7, 7];
+
+        assert_eq!(vec![0, 1, 2, 1, 2, 3, 3], minimum_paths(n, &shortcuts));
+    }
 }
