0251c7a9b3aff24a1e70a9a6192a7e681c1c4213
jeandeducla <jean.deducla@gmail.com>
2022-05-12 16:49:27 +0200

    basic code structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn minimum_path(target: usize, shortcuts: &[usize]) -> usize {
+    0
+}
+
+fn minimum_paths(n: usize, shortcuts: &[usize]) -> Vec<usize> {
+    let mut minimum_paths = Vec::new();
+    for i in 0..n {
+        let min = minimum_path(i, &shortcuts);
+        minimum_paths.push(min);
+    }
+    minimum_paths
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = 3;
+    let shortcuts = vec![2, 2, 3];
+
+    println!("mike's minimum paths: {:?}", minimum_paths(n, &shortcuts));
 }
